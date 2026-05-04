510e7e0a3f6923a9f630f527c8a996346703bf1c
Joga Nubaid <jonub2503832@gmail.com>
2026-05-04 21:41:16 +0530

    fix: add compatibility config for patches

--- patches/src/main/kotlin/org/cryptomator/patches/Constants.kt
+++ patches/src/main/kotlin/org/cryptomator/patches/Constants.kt
@@ -1,7 +1,27 @@
 package org.cryptomator.patches
 
+import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
+
 object Constants {
-    // Cryptomator version 1.12.3
     const val APP_PACKAGE = "org.cryptomator"
     const val APP_VERSION = "1.12.3"
+
+    val COMPATIBILITY = Compatibility(
+        name = "Cryptomator",
+        packageName = APP_PACKAGE,
+        apkFileType = ApkFileType.APK_REQUIRED,
+        appIconColor = 0xFF4758A0,
+        signatures = setOf(
+            // Add signature hashes here when known
+            // For now, we'll try without specific signatures
+        ),
+        targets = listOf(
+            AppTarget(
+                version = "1.12.3",
+                minSdk = 26
+            )
+        )
+    )
 }
